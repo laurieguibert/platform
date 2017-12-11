5c929bfce7ba96062674bb92dac60ae18a077a46
Laurie <l.guibert@hotmail.fr>
2017-12-11 19:43:45 +0100

    add getByDurationAndDurationType query

--- src/AppBundle/Repository/LessonRepository.php
+++ src/AppBundle/Repository/LessonRepository.php
@@ -10,4 +10,14 @@ namespace AppBundle\Repository;
  */
 class LessonRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function getByDurationAndDurationType($duration, $type){
+        $qb = $this->createQueryBuilder('l')
+            ->where('l.duration = :duration')
+            ->andWhere('l.duration_type = :durationType')
+            ->setParameter('duration', $duration)
+            ->setParameter('durationType', $type)
+            ->getQuery();
+
+        return $qb->execute();
+    }
 }

8197b94b5cf7f45ccd70c3bf013bb2d8401e311b
Laurie <l.guibert@hotmail.fr>
2017-12-11 16:39:57 +0100

    Add tests directory

---
+++ src/AppBundle/Tests/Controller/UserControllerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+    public function testShowUSer()
+    {
+        $client = static::createClient();
+
+        //$crawler = $client->request('GET', '/users');
+        $crawler = $client->getResponse()->getContent();
+
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"'
+        );
+
+        $this->assertContains('users', $crawler);
+        $this->assertTrue($client->getResponse()->isNotFound());
+    }
+
+    public function testAddUser(){
+        $client = static::createClient();
+
+        $crawler = $client->getResponse()->getContent();
+
+        $client->request(
+            'POST',
+            '/user/new',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            '{"username":"usertest", "password":"usertest", "email":"user@mail.fr"}'
+        );
+    }
+}

208b395be9bc17eea81810705bb19aca86848b10
Laurie <l.guibert@hotmail.fr>
2017-12-12 11:55:49 +0100

    create new custom serializer to serialize many to many relation entity - see indexAction() of Sector

--- src/AppBundle/Serializer/Normalizer/customNormalizer.php
+++ src/AppBundle/Serializer/Normalizer/customNormalizer.php
@@ -1,12 +1,21 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: lguib
- * Date: 12/12/2017
- * Time: 10:48
- */
-class customNormalizer
-{
+namespace AppBundle\Serializer\Normalizer;
 
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\Component\PropertyInfo\PropertyTypeExtractorInterface;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactoryInterface;
+use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
+
+class customNormalizer extends ObjectNormalizer
+{
+    public function __construct(ClassMetadataFactoryInterface $classMetadataFactory = null, NameConverterInterface $nameConverter = null, PropertyAccessorInterface $propertyAccessor = null, PropertyTypeExtractorInterface $propertyTypeExtractor = null)
+    {
+        parent::__construct($classMetadataFactory, $nameConverter, $propertyAccessor, $propertyTypeExtractor);
+
+        $this->setCircularReferenceHandler(function ($object) {
+            return $object->getId();
+        });
+    }
 }

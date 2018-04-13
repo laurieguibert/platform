93cf558fbaba0ec0f84248bd27c6d934f02c5e9f
laurieguibert <l.guibert@hotmail.fr>
2018-04-13 17:18:29 +0200

    Add Subscription entity

---
+++ src/AppBundle/Entity/Subscription.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Subscription
+ *
+ * @ORM\Table(name="subscription")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SubscriptionRepository")
+ */
+class Subscription
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=50, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal", precision=5, scale=2, unique=true)
+     */
+    private $price;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nbMaxLesson", type="integer")
+     */
+    private $nbMaxLesson;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Subscription
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     *
+     * @return Subscription
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set nbMaxLesson
+     *
+     * @param integer $nbMaxLesson
+     *
+     * @return Subscription
+     */
+    public function setNbMaxLesson($nbMaxLesson)
+    {
+        $this->nbMaxLesson = $nbMaxLesson;
+
+        return $this;
+    }
+
+    /**
+     * Get nbMaxLesson
+     *
+     * @return int
+     */
+    public function getNbMaxLesson()
+    {
+        return $this->nbMaxLesson;
+    }
+}
+

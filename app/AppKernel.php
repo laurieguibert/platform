e3b29a89c0b0981b874a5cbf71db13af9c1ba366
laurieguibert <l.guibert@hotmail.fr>
2018-05-16 10:11:48 +0200

    Add Bootstrap and Jquery to project

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Vich\UploaderBundle\VichUploaderBundle(),
             new JMS\Payment\CoreBundle\JMSPaymentCoreBundle(),
             new JMS\Payment\PaypalBundle\JMSPaymentPaypalBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {

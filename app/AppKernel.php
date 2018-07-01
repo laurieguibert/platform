8764fd1e82dd5e2997d7418c8dfb5a51f3adbea7
Gianni Azizi <gianni.azizi@bnpparibas-pf.com>
2018-07-01 18:07:59 +0200

    Fix heroku fixtures

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,6 +36,10 @@ class AppKernel extends Kernel
             }
         }
 
+        if (in_array($this->getEnvironment(), array('prod'))) {
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+        }
+
         return $bundles;
     }
 
